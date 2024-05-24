f714f6b3abbe953f20e72ab338301db17b206440
Quentindeve <quentindutilleul@pm.me>
2024-05-24 19:51:46 +0200

    handle missing commands

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,23 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
     io::stdout().flush().unwrap();
 
+    let commands: Vec<String> = vec![];
+    loop {
+        print!("$ ");
+        let input = prompt_input();
+        if !commands.contains(&input) {
+            println!("{}: command not found", &input.trim());
+        }
+    }
     // Wait for user input
+}
+
+fn prompt_input() -> String {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    input
 }
