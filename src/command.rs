e42fa97c0f2b75b5766d5271c6ce169847acd62e
Quentindeve <quentindutilleul@pm.me>
2024-05-24 20:53:43 +0200

    oopsie ?

---
+++ src/command.rs
@@ -0,0 +1,24 @@
+pub type CommandCallback = fn(Vec<String>) -> Result<(), ErrorType>;
+
+use crate::builtins::*;
+
+pub enum ErrorType {
+    CommandNotFound(String),
+    ArgumentError(usize),
+    NotNullExitCode(i32),
+}
+
+pub struct BuiltinCommand<'a> {
+    pub name: &'a str,
+    pub callback: CommandCallback,
+}
+
+impl<'a> BuiltinCommand<'a> {
+    pub fn execute(&self, args: Vec<String>) -> Result<(), ErrorType> {
+        (self.callback)(args)
+    }
+}
+
+pub fn get_builtins() -> Vec<BuiltinCommand<'static>> {
+    vec![BUILTIN_EXIT]
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+pub mod builtins;
+pub mod command;
+
 fn main() {
     io::stdout().flush().unwrap();
 
-    let commands: Vec<String> = vec![];
+    let builtins = command::get_builtins();
+
     loop {
         let input = prompt_input();
-        if !commands.contains(&input) {
-            println!("{}: command not found", &input.trim());
+        let args = input
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
+
+        if args.len() == 0 {
+            continue;
+        }
+
+        let command = builtins
+            .iter()
+            .find(|builtin| builtin.name == args.get(0).unwrap());
+
+        match command {
+            Some(builtin) => {
+                let result = builtin.execute(args);
+            }
+            None => {
+                println!("{}: command not found", &input.trim());
+            }
         }
     }
     // Wait for user input
